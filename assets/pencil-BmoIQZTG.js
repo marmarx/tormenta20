f99d05e4bd713a0fbad316e46a4a77c46be0ecc6
marmarx <78326372+marmarx@users.noreply.github.com>
2025-04-02 14:46:29 -0300

    Delete assets directory

--- assets/pencil-BmoIQZTG.js
+++
@@ -1,1 +0,0 @@
-import{_ as c,c as s,o,a as n}from"./index-B9l4Vmpu.js";const t={},a={class:"pencilbox"};function r(d,e){return o(),s("div",a,e[0]||(e[0]=[n("div",{class:"pencil"},null,-1)]))}const _=c(t,[["render",r],["__scopeId","data-v-d3ed7c8e"]]);export{_ as default};
